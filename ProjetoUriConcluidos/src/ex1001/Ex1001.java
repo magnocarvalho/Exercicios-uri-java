b7f1b6852d87778cef237f4ac1a758bacd344879
magno <magno@DESKTOP-UL8REEO>
2017-12-06 02:20:29 -0200

    Exercicio 1001 uri concluido

---
+++ ProjetoUriConcluidos/src/ex1001/Ex1001.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex1001;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author magno
+ */
+public class Ex1001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        System.out.println("X = "+ (a + b));
+    }
+    
+}
