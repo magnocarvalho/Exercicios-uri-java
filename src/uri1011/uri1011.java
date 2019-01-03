5f74ab69c349d5add6517309aed7d6f7e075f128
Magno Carvalho <magnocarv@hotmail.com>
2019-01-03 07:08:31 -0200

    uri 1011

--- src/uri1011/uri1011.java
+++ src/uri1011/uri1011.java
@@ -5,10 +5,26 @@
  */
 package uri1011;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Scanner;
+
 /**
  *
  * @author magno
  */
 public class uri1011 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger i = new BigInteger("0");
+        i = sc.nextBigInteger();
+        sc.close();
+        i = i.pow(3);
+        BigDecimal temp = new BigDecimal(i);
+        BigDecimal a = new BigDecimal(4 / 3.0);
+        BigDecimal b = a.multiply(temp);
+        BigDecimal c = b.multiply(new BigDecimal("3.14159"));
+        System.out.printf("VOLUME = %.3f\n", c);
+    }
 }
